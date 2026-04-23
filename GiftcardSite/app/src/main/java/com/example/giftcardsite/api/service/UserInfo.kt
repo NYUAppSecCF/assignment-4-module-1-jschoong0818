18d1c06ff5e47aba183b17f963cef93fdfbd8256
jc13012@nyu.edu <jc13012@nyu.edu>
2026-04-23 10:46:11 +0800

    10. Part 4 Trial

--- GiftcardSite/app/src/main/java/com/example/giftcardsite/api/service/UserInfo.kt
+++ GiftcardSite/app/src/main/java/com/example/giftcardsite/api/service/UserInfo.kt
@@ -1,13 +1,3 @@
 package com.example.giftcardsite.api.service
 
-import com.example.giftcardsite.api.model.User
-import com.example.giftcardsite.api.model.UserInfoContainer
-import retrofit2.Call
-import retrofit2.http.Body
-import retrofit2.http.Header
-import retrofit2.http.POST
-
-interface UserInfo {
-    @POST("/api/metrics")
-    fun postInfo(@Body info: UserInfoContainer, @Header("Authorization") token: String?) : Call<User>
-}
+// Fix by Jin-Shen: Removed this entire file as it was used exclusively to POST metrics location and sensor data to /api/metrics. This functionality has been removed to respect user privacy.
